a58dc06527af08c07dcbfb17dd814fbbae5ff1be
Saad Ahmed // سعد احمد <i221084@nu.edu.pk>
2025-05-04 13:48:53 +0500

    Update ciphers.py
    added decryption functionality

--- scientific-computing/ciphers.py
+++ scientific-computing/ciphers.py
@@ -17,23 +17,31 @@ def caesar(message, key):
     
 caesar(text, shift)
 
-def vigenere(message, key):
+def vigenere(message, key, direction=1):
     # For more info on this cypher: https://chatgpt.com/c/6814deea-bfd0-800a-890f-91eb199f52b9
     key_index = 0
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    encrypted_text = ''
+    final_message = ''
 
     for char in message.lower():
-    
-        if char == ' ':
-            encrypted_text += char
+        # Append any non-letter character to the message
+        if not char.isalpha():
+            final_message += char
         else:        
+            # Find the right key character to encode/decode
             key_char = key[key_index % len(key)]
             key_index += 1
 
+            # Define the offset and the encrypted/decrypted letter
             offset = alphabet.index(key_char)
             index = alphabet.find(char)
-            new_index = (index + offset) % len(alphabet)
-            encrypted_text += alphabet[new_index]
+            new_index = (index + offset*direction) % len(alphabet)
+            final_message += alphabet[new_index]
     
-    return encrypted_text
+    return final_message
+
+def encrypt(message, key):
+    return vigenere(message, key)
+    
+def decrypt(message, key):
+    return vigenere(message, key, -1)

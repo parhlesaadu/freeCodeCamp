6f92563e744a43bf0ca491cfc2f1d30114e5063e
Saad Ahmed // سعد احمد <i221084@nu.edu.pk>
2025-08-04 12:24:07 +0500

    Update case-converter.py
    added list comprehension + docs

--- python/scientific-computing/case-converter.py
+++ python/scientific-computing/case-converter.py
@@ -1,12 +1,18 @@
 def convert_to_snake_case(pascal_or_camel_cased_string):
     snake_cased_char_list = []
+    #   append processed characters one by one
     for char in pascal_or_camel_cased_string:
         if char.isupper():
           converted_character = '_' + char.lower()
           snake_cased_char_list.append(converted_character)
         else:
             snake_cased_char_list.append(char)
+    #  final processing of the list (python strings are immutable, so each method returns a new string object)
     snake_cased_string = ''.join(snake_cased_char_list)   # returns the concatenation of all strings in the iterable using the calling string as a separator
     clean_snake_cased_string = snake_cased_string.strip('_')    # returns new string with parameter str removed from start and end of calling str
 
     return clean_snake_cased_string
+
+def convert_to_snake_case_list_comprehension(pascal_or_camel_cased_string):
+    snake_cased_char_list = ['_' + char.lower() if char.isupper() else char for char in pascal_or_camel_cased_string]    # if no else clause, if statement goes after 'for x in y'
+    return ''.join(snake_cased_char_list).strip('_')
